656fa96a848d4bc46f3fd43384d318641e992a9d
Alfy26 <alfonsomartino@MacBook-Air-di-Alfonso.local>
2016-04-29 00:29:19 +0200

    provachiusura

--- app/src/main/java/com/example/marti/laboratorio2/MainActivity.java
+++ app/src/main/java/com/example/marti/laboratorio2/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     String JsonObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        //ciao
         super.onCreate(savedInstanceState);
         SharedPreferences app_preferences = PreferenceManager.getDefaultSharedPreferences(this);
         JsonObject = app_preferences.getString("json",loadJSONFromAsset());
